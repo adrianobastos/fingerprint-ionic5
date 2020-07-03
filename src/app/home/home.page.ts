e9962e0239372be73407da3031aababe61dec62f
Adriano Bastos <adriano.bastos@araisan.io>
2020-07-03 10:35:14 -0300

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { FingerprintOptions} from '@ionic-native/fingerprint-aio';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,29 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  fingerprintOptions : FingerprintOptions;
+
+  constructor(
+    public navCtrl: NavController,
+    public fingerAuth: FingerprintAIO
+  ){  }
+
+  ngOnInit() {
+  }
+
+  public showFingerprintAuthDlg(){
+      this.fingerprintOptions = {
+          clientId:'fingerprint-demo',
+          clientSecret: 'password',
+          disableBackup: true
+      }
+      this.fingerAuth.isAvailable().then(result =>{
+        if(result === "OK"){
+            this.fingerAuth.show(this.fingerprintOptions)
+            .then((result: any) => console.log(result))
+            .catch((error: any) => console.log(error));
+        }
+      });
+  }
 
 }
